75526224a3761b45283ec8f861f7cecbcfef233c
maikon <maikon@gmail.com>
2020-10-19 16:24:22 -0400

    Aula 01 - Criação do projeto e view template

---
+++ resources/views/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.principal')
+
+@section('main')
+
+<div class="container">
+  <div class="py-5 text-center">
+    <h2>Produtos</h2>
+  </div>
+  <div class="row">
+
+    <!-- Departamentos -->
+    <div class="col-md-4" >
+      <h3>Departamentos</h3>
+      {{-- TODO --}}
+    </div>
+
+    <!-- Marcas -->
+    <div class="col-md-4" >
+      <h3>Marcas</h3>
+      {{-- TODO --}}
+    </div>
+
+    <!-- Produtos -->
+    <div class="col-md-4" >
+      <h3>Produtos</h3>
+      {{-- TODO --}}
+    </div>
+
+  </div>
+</div>
+
+
+
+@endsection
