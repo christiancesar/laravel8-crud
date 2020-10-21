afeda7091116d2e280ed7e00cef04ccd378f508a
maikon <maikon@gmail.com>
2020-10-21 05:39:48 -0400

    Aula 04 - Arrumando rota para index e criando rota para departamentos

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\DepartamentoController;
+use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', IndexController::class)->name('index');
+Route::resource('departamentos', DepartamentoController::class);
