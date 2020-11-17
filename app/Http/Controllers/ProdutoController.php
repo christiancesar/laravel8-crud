57430561d56e78891259dc5e24ac591316883a01
maikon <maikon@gmail.com>
2020-11-17 05:47:50 -0400

    Aula 17 - Produtos - Create

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Departamento;
+use App\Models\Marca;
 use App\Models\Produto;
 use Illuminate\Http\Request;
 
@@ -25,7 +27,9 @@ class ProdutoController extends Controller
      */
     public function create()
     {
-        //
+        $marcas = Marca::all();
+        $departamentos = Departamento::all();
+        return view('produtos.create', compact(['marcas', 'departamentos']));
     }
 
     /**
